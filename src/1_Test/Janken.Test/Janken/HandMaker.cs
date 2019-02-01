6e04e4417a141710abc69704e55e7c82d87a0620
ytyaru <ytyaru@outlook.jp>
2019-02-01 17:07:51 +0900

    Created Game class.

--- src/1_Test/Janken.Test/Janken/HandMaker.cs
+++ src/1_Test/Janken.Test/Janken/HandMaker.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 namespace Janken
 {
     public class HandMaker {
+        public static HandMaker Instance { get; } = new HandMaker();
+        private HandMaker() {}
+
         /// <summary>ランダムな手を返す</summary>
         /// <returns>ランダムな手</returns>
         public Hands.Unicode Random() {            
